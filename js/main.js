5292feea05d1fa388ddd504a9a0d1aa2665ec0c9
Arvind <arvindkhadri@gmail.com>
2012-11-23 19:02:20 +0530

    Merging code for differentiating content based on tags

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-
+var template = _.template($('#image-template').html());
+$("#select").on('change',onSelectChange);
+M = {};
+$.getJSON("http://localhost:60/js/maraa.json",function(data){
+		M = data;
+		_.each(data.image_list, function(item) {
+        if(item.tag == "CR")
+						x = $('#imgs').append(template({
+								img: item.image
+						}))});
+});
+function onSelectChange(e){
+		$("#imgs").html('');
+		_.each(M.image_list,  function(item) {
+        if(item.tag == e.currentTarget.value)
+						x = $('#imgs').append(template({
+								img: item.image
+						}));});
+}
