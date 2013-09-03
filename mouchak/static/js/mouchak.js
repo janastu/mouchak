ec2b20b4a1c49eecdf25dcc39f08bb53b729932c
Anon Ray <rayanon004@gmail.com>
2013-09-03 11:10:01 +0530

    Fix initial page to load

--- mouchak/static/js/mouchak.js
+++ mouchak/static/js/mouchak.js
@@ -174,8 +174,8 @@ M.init = function() {
   var app_router = new AppRouter();
   Backbone.history.start();
 
-  if(!location.hash) {
-    var startpage = M.pages.models[0].get('name');
+  if(!window.location.hash) {
+    var startpage = M.pages.site_content.menu.menuOrder[0];
     app_router.navigate(startpage, {trigger: true});
   }
 
