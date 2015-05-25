ecd08578607ec9978d1ce6a792588299ed8d6681
Arvind <arvindkhadri@gmail.com>
2015-05-25 14:53:30 +0530

    Fix SON manipulation
    Check if _id is a key in the given dictionary, it might be absent when
    making counts on a cursor object.

--- mouchak/utilities.py
+++ mouchak/utilities.py
@@ -4,6 +4,7 @@ from pymongo.son_manipulator import SONManipulator
 class ObjectIdCleaner(SONManipulator):
     def transform_outgoing(self, son, collection):
         """Change the _id to be a string instance"""
-        son['id'] = str(son['_id'])
-        del(son['_id'])
-        return son
+        if '_id' in son:
+            son['id'] = str(son['_id'])
+            del(son['_id'])
+            return son
