61dfb549c9f5416ef15bb3ea04b719a06ea7c01e
Arvind <arvindkhadri@gmail.com>
2015-05-28 19:48:13 +0530

    Set date in SONManipulator
    Set date to epoch if a database entry has the date missing. Having a
    blank date entry stops elasticsearch from parsing the date field correctly.

--- mouchak/utilities.py
+++ mouchak/utilities.py
@@ -7,4 +7,6 @@ class ObjectIdCleaner(SONManipulator):
         if '_id' in son:
             son['id'] = str(son['_id'])
             del(son['_id'])
+        if 'date' in son and son['date'] == '':
+            son['date'] = "1970-01-01"
         return son
